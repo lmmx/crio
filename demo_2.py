9422a8f9f29f59ee53aa40e0950d88de5d307e2b
Louis Maddox <louismmx@gmail.com>
2025-01-30 23:36:31 +0000

    test: longer running test

--- demo_2.py
+++ demo_2.py
@@ -1,16 +1,25 @@
 import crio
 
+with crio.checkpoint():
+    print("Inside checkpoint context")
+    # Do something here
+    print("SETTING x = 42")
+    x = 42
+    print("IMPORT DATETIME.DATETIME")
+    from datetime import datetime
 
-def main():
-    print("Starting demo")
-    with crio.checkpoint():
-        print("Inside checkpoint context")
-        # Do something here
-        x = 42
-        print(f"x = {x}")
+    print("IMPORT TORCH, TRANSFORMERS, PYDANTIC")
+    t1 = datetime.now()
+    from pydantic import BaseModel, TypeAdapter, create_model
+    from torch import cuda
+    from transformers import AutoModel, AutoTokenizer
 
-    print("Checkpoint completed successfully")
+    t2 = datetime.now()
+    print(f"IMPORTS TOOK {(t2-t1).seconds} SECONDS")
 
-
-if __name__ == "__main__":
-    main()
+x
+print("Checkpoint completed successfully")
+cuda.is_available()
+datetime.now().isoformat()
+AutoModel
+create_model("DataModel")
